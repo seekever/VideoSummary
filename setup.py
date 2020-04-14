a8a8960af26e965512d97fd52357b59f6973cc57
Daniel Turner Cebriá <untalturner@gmail.com>
2020-04-14 22:21:00 +0200

    Hello world.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='VideoSummary',
+	version='0.1',
+	packages=['video_summary', 'video_summary.test'],
+	url='https://github.com/untalturner/VideoSummary',
+	license='LICENSE.txt',
+	author='Daniel Turner Cebriá',
+	author_email='daniel.turner.cebria@gmail.com',
+	description='Python-based program to summarize videos from subtitle analysis and object detection.  '
+)
