ea77a5baa43b1279b1281e24b01d6a6db95057a7
Dani Turner <untalturner@gmail.com>
2020-05-24 20:05:27 +0200

    Beautify ResumeOptions.ui

--- video_summary/models/resume_options_model.py
+++ video_summary/models/resume_options_model.py
@@ -5,7 +5,7 @@ import os
 
 from PyQt5 import QtWidgets, uic
 
-from video_summary.context.general_context import GeneralContext
+from video_summary.context.general_context import GeneralContext, ResumeMode
 from video_summary.context.objects_context import ObjectsContext
 from video_summary.context.scenes_context import ScenesContext
 from video_summary.context.subtitles_context import SubtitlesContext
@@ -34,11 +34,22 @@ class ResumeOptions(QtWidgets.QMainWindow, ModelInterface):
     ----------
     configurations : dict
         the dict with all the context configs
+    detect_scenes : bool
+        a boolean to activate the detect scenes' progress bar
+    object_analysis : bool
+        a boolean to activate the object analysis' progress bar
+    subtitle_analysis : bool
+        a boolean to activate the subtitle analysis' progress bar
 
     """
 
     # Context config
     configurations = dict()
+
+    # Progress bars visibility
+    detect_scenes = True
+    object_analysis = True
+    subtitle_analysis = True
 
     def __init__(self, *args, **kwargs):
         LOG.debug('initializing resume options window model')
@@ -67,6 +78,11 @@ class ResumeOptions(QtWidgets.QMainWindow, ModelInterface):
         LOG.debug('loading contexts')
         with GeneralContext(read_only=True) as manager:
             self.configurations["General"] = manager.config
+            self.detect_scenes = manager.detect_scenes
+            self.object_analysis = manager.resume_mode in (ResumeMode.OBJECTS,
+                                                           ResumeMode.SUBTITLES_AND_OBJECTS)
+            self.subtitle_analysis = manager.resume_mode in (ResumeMode.SUBTITLES,
+                                                             ResumeMode.SUBTITLES_AND_OBJECTS)
         with ScenesContext(read_only=True) as manager:
             self.configurations["Subtitles"] = manager.config
         with ObjectsContext(read_only=True) as manager:
@@ -77,6 +93,13 @@ class ResumeOptions(QtWidgets.QMainWindow, ModelInterface):
         self.resumeOptionsLabel.setText(json.dumps(self.configurations, indent=4))
 
         LOG.debug('contexts loaded')
+
+    def reload_conditional_format(self):
+        LOG.debug('reloading conditional format')
+        self.scenesProgressWidget.setVisible(self.detect_scenes)
+        self.objectsProgressWidget.setVisible(self.object_analysis)
+        self.subtitlesProgressWidget.setVisible(self.subtitle_analysis)
+        LOG.debug('conditional format reloaded')
 
     def next_window(self):
         super().next_window()
